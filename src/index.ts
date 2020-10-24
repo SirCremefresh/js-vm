75017761f930950585428a0f08b66ef3aa2573b9
SirCremefresh <donato.wolfisberg@gmail.com>
2020-10-24 15:10:41 +0200

    added jl instruction

--- src/index.ts
+++ src/index.ts
@@ -2,41 +2,18 @@ import {Instruction} from './instructions';
 import {Register} from './register';
 import {Vm} from './vm';
 
-//
-// [:|] mit inc instruction
-// [:)]
-// if (process.env.NODE_ENV !== 'production') {
 console.time('vm_test');
-// }
+
 const vm = new Vm(Int32Array.from([
   Instruction.INSTRUCTION_PUSH, 1_000_000_000,
   Instruction.INSTRUCTION_LOAD, Register.D,
   Instruction.INSTRUCTION_LOG, Register.D,
   Instruction.INSTRUCTION_LABEL,
   Instruction.INSTRUCTION_INC, Register.C, 1,
-  Instruction.INSTRUCTION_JLE, 6,
+  Instruction.INSTRUCTION_JL, 6,
   Instruction.INSTRUCTION_HALT
 ]));
 
-
-// const vm = new Vm([
-// 	Instruction.INSTRUCTION_PUSH, 1,
-// 	Instruction.INSTRUCTION_LOAD, Register.C,
-// 	Instruction.INSTRUCTION_LOG, Register.C,
-// 	Instruction.INSTRUCTION_HALT
-// ]);
-//
-
-//
-// const vm = new Vm([
-// 	Instruction.INSTRUCTION_ADD, 2, 3,
-// 	Instruction.INSTRUCTION_INC, Register.C, 4,
-// 	Instruction.INSTRUCTION_INC, Register.C, 5,
-// 	Instruction.INSTRUCTION_LOG, Register.C,
-// 	Instruction.INSTRUCTION_HALT
-// ]);
-
 vm.run();
-// if (process.env.NODE_ENV !== 'production') {
+
 console.timeLog('vm_test');
-// }
---
+++ src/instructions/jl.ts
@@ -0,0 +1,12 @@
+import {Register} from '../register';
+import {VmState} from '../vm-state';
+import {jmp} from './jmp';
+
+export function jl(vmState: VmState): void {
+  if (vmState.registers[Register.C] >= vmState.registers[Register.D]) {
+    vmState.instructionIndex++;
+    return;
+  }
+
+  jmp(vmState);
+}
