eb997074b6f7ec3e4e3cd2cd9251fd841cf00e0c
SirCremefresh <donato.wolfisberg@gmail.com>
2020-11-03 16:44:05 +0100

    added movable cursor

--- src/app/sample-lexer-program/index.ts
+++ src/app/sample-lexer-program/index.ts
@@ -8,59 +8,57 @@ function onDomReady(fn: () => void) {
   }
 }
 
-function textWidth(text: string, fontProp: string): number {
-  const tag = document.createElement('div');
-  tag.style.position = 'absolute';
-  tag.style.left = '-999em';
-  tag.style.whiteSpace = 'nowrap';
-  tag.style.font = fontProp;
-  tag.innerHTML = text;
+const fontLengthScaling = 0.6;
+const fontHeightScaling = 1.25;
 
-  document.body.appendChild(tag);
+let fontLength = 0;
+let fontHeight = 0;
 
-  const result = tag.clientWidth;
-
-  document.body.removeChild(tag);
-
-  return result;
-}
-
-let characterWith: number;
-let asdf = 0;
+let cursorX = 0;
+let cursorY = 0;
 
 onDomReady(() => {
   const editor = document.getElementById('editor');
-  characterWith = textWidth('C', 'JetBrains Mono 1em') - 1;
+  const cursor = document.querySelector('.cursor') as HTMLElement;
+
+  const fontSize = parseInt(
+    getComputedStyle(document.documentElement)
+      .getPropertyValue('--editor-font-size')
+  );
+
+  fontLength = fontSize * fontLengthScaling;
+  fontHeight = fontSize * fontHeightScaling;
+
+  console.log({fontSize, fontLength, fontHeight});
 
   if (editor != null) {
-    editor.addEventListener('click', e => {
-      const position = window.getSelection()?.focusOffset;
-      if (position === undefined) {
-        console.log('could not get position');
-        return null;
+    const offsetTop = editor.offsetTop;
+    const offsetLeft = 9;
+
+    document.addEventListener('keydown', event => {
+      event.preventDefault();
+
+      // if (event.isComposing || event.key === 229) {
+      //   return;
+      // }
+      if (event.key === 'ArrowRight') {
+        cursorX++;
       }
-      console.log(e.target);
-      console.log(position);
-      console.log(e);
-
-      console.log(editor.childNodes);
-      const cursor = editor.querySelectorAll('.line-number')[0].querySelector('.cursor');
-      console.log(characterWith);
-      console.log(asdf);
-      asdf += 1;
-      (cursor as HTMLElement).style.left = `${asdf * 6}px`;
-      console.dir((cursor as HTMLElement).style.left);
-
-      // if (cursor && position > cursor)
-      //   position--;
-      // if (clean)
-      //   editor['innerText'] = clean;
-      // const textnode = (editor.firstChild['splitText'] as any)(position);
-      // clean = textnode.wholeText;
-      // cursor = position;
-      // editor.insertBefore(document.createTextNode('|'), textnode);
-      // editor['innerText'] = textnode.wholeText;
+      if (event.key === 'ArrowLeft') {
+        cursorX--;
+      }
+      if (event.key === 'ArrowDown') {
+        cursorY++;
+      }
+      if (event.key === 'ArrowUp') {
+        cursorY--;
+      }
+      console.log(event.key);
+      cursor.style.top = `${offsetTop + cursorY * fontHeight}px`;
+      cursor.style.left = `${offsetLeft + cursorX * fontLength}px`;
     });
+
+
     console.log('added listener');
   }
   console.log('after');
@@ -89,3 +87,39 @@ console.log(tokenStream.map(tokenElement => ({
 })));
 console.log(programText.length);
 console.log(tokenStream[tokenStream.length - 1].endIndex - programText.length == -1 ? 'passed' : 'failed');
+
+
+// Lang 6
+// Hoch 12.5
+
+// Lang 24
+// Hoch 25
+// editor.addEventListener('click', e => {
+//   const position = window.getSelection()?.focusOffset;
+//   if (position === undefined) {
+//     console.log('could not get position');
+//     return null;
+//   }
+//   console.log(e.target);
+//   console.log(position);
+//   console.log(e);
+//
+//   console.log(editor.childNodes);
+//   const cursor = document.querySelector('.cursor') as HTMLElement;
+//   console.log(characterWith);
+//   console.log(asdf);
+//   asdf += 1;
+//   (cursor as HTMLElement).style.top = `${offsetTop}px`;
+//   (cursor as HTMLElement).style.left = `${offsetLeft + asdf * 6}px`;
+//   console.dir((cursor as HTMLElement).style.left);
+//
+//   // if (cursor && position > cursor)
+//   //   position--;
+//   // if (clean)
+//   //   editor['innerText'] = clean;
+//   // const textnode = (editor.firstChild['splitText'] as any)(position);
+//   // clean = textnode.wholeText;
+//   // cursor = position;
+//   // editor.insertBefore(document.createTextNode('|'), textnode);
+//   // editor['innerText'] = textnode.wholeText;
+// });
