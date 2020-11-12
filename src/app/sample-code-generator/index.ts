57d6a0b539b441a7928419af1e09539209d9f2cf
SirCremefresh <donato.wolfisberg@gmail.com>
2020-11-12 19:47:13 +0100

    added simple code-generator-example

---
+++ src/app/sample-code-generator/index.ts
@@ -0,0 +1,99 @@
+import { getTokenStream, Token, TokenElement } from '../../lib/lexer/lexer';
+import { Instruction, Register, Vm } from '../../lib/vm-runtime';
+
+const programText = `
+push 100000
+load %rd
+log %rd
+
+halt`;
+
+const tokenStream = getTokenStream(programText);
+
+function getCodeFromToken(tokenElement: TokenElement): string {
+  return programText.slice(tokenElement.startIndex, tokenElement.endIndex + 1);
+}
+
+function fail(message: string): never {
+  console.error(message);
+  process.exit(1);
+}
+
+const InstructionHandlers = {
+  push: (tokenElement: TokenElement) => {
+    return Instruction.INSTRUCTION_PUSH;
+  },
+  load: (tokenElement: TokenElement) => {
+    return Instruction.INSTRUCTION_LOAD;
+  },
+  log: (tokenElement: TokenElement) => {
+    return Instruction.INSTRUCTION_LOG;
+
+  },
+  halt: (tokenElement: TokenElement) => {
+    return Instruction.INSTRUCTION_HALT;
+  }
+};
+
+type InstructionHandler = keyof typeof InstructionHandlers;
+
+function isInstruction(code: string | InstructionHandler): code is InstructionHandler {
+  return code in InstructionHandlers;
+}
+
+const TokenHandlers = {
+  [Token.TOKEN_INSTRUCTION]: (tokenElement: TokenElement) => {
+    const instructionCode = getCodeFromToken(tokenElement);
+    if (isInstruction(instructionCode)) {
+      return [InstructionHandlers[instructionCode](tokenElement)];
+    } else {
+      fail(`Instruction not recognised: ${instructionCode}. location: ${tokenElement.startIndex}`);
+    }
+  },
+  [Token.TOKEN_COMMENT]: (tokenElement: TokenElement) => {
+    return [];
+  },
+  [Token.TOKEN_WHITESPACE]: (tokenElement: TokenElement) => {
+    return [];
+  },
+  [Token.TOKEN_INVALID]: (tokenElement: TokenElement) => {
+    fail(`Invalid. location: ${tokenElement.startIndex}`);
+  },
+  [Token.TOKEN_LABEL]: (tokenElement: TokenElement) => {
+    return [];
+  },
+  [Token.TOKEN_COMMA]: (tokenElement: TokenElement) => {
+    return [];
+  },
+  [Token.TOKEN_REGISTER]: (tokenElement: TokenElement) => {
+    const code = getCodeFromToken(tokenElement);
+    switch (code) {
+    case '%rd':
+      return [Register.D];
+    default:
+      fail(`Register is not known: ${code}. location: ${tokenElement.startIndex}`);
+    }
+    return [];
+  },
+  [Token.TOKEN_CONST]: (tokenElement: TokenElement) => {
+    return [parseInt(getCodeFromToken(tokenElement))];
+  },
+  [Token.TOKEN_STACK]: (tokenElement: TokenElement) => {
+    return [];
+  },
+  [Token.TOKEN_NEWLINE]: (tokenElement: TokenElement) => {
+    return [];
+  }
+};
+
+const result = [];
+for (const tokenElement of tokenStream) {
+  result.push(...TokenHandlers[tokenElement.token](tokenElement));
+}
+
+console.log(tokenStream);
+console.log(result);
+
+const instructions = Int32Array.from(result);
+const vm = new Vm(instructions);
+vm.run();
