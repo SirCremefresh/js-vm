b4cb6e9851989431d416276f23eb6d88ad53e09e
SirCremefresh <donato.wolfisberg@gmail.com>
2020-11-12 21:10:11 +0100

    @sirh3e hello how is day going.
    added first simple test of code - generator

--- src/app/sample-code-generator/index.ts
+++ src/app/sample-code-generator/index.ts
@@ -1,15 +1,6 @@
 import { getTokenStream, Token, TokenElement } from '../../lib/lexer/lexer';
 import { Instruction, Register, Vm } from '../../lib/vm-runtime';
 
-const programText = `
-push 100000
-load %rd
-log %rd
-
-halt`;
-
-const tokenStream = getTokenStream(programText);
-
 function getCodeFromToken(tokenElement: TokenElement): string {
   return programText.slice(tokenElement.startIndex, tokenElement.endIndex + 1);
 }
@@ -19,19 +10,45 @@ function panic(message: string): never {
   process.exit(1);
 }
 
-const InstructionHandlers = {
-  push: (tokenElement: TokenElement) => {
-    return Instruction.INSTRUCTION_PUSH;
-  },
-  load: (tokenElement: TokenElement) => {
-    return Instruction.INSTRUCTION_LOAD;
-  },
-  log: (tokenElement: TokenElement) => {
-    return Instruction.INSTRUCTION_LOG;
+function parseRegistry(regString: string): number {
+  switch (regString) {
+  case '%ra':
+    return Register.A;
+  case '%rb':
+    return Register.B;
+  case '%rc':
+    return Register.C;
+  case '%rd':
+    return Register.D;
+  default:
+    panic(`Register is not known: ${regString}`);
+  }
+}
 
+const InstructionHandlers = {
+  push: (generatorState: GeneratorState) => {
+    const [, constant] = generatorState.line;
+    return [Instruction.INSTRUCTION_PUSH, parseInt(getCodeFromToken(constant))];
   },
-  halt: (tokenElement: TokenElement) => {
-    return Instruction.INSTRUCTION_HALT;
+  load: (generatorState: GeneratorState) => {
+    const [, par1] = generatorState.line;
+    return [Instruction.INSTRUCTION_LOAD, parseRegistry(getCodeFromToken(par1))];
+  },
+  log: (generatorState: GeneratorState) => {
+    const [, par1] = generatorState.line;
+    return [Instruction.INSTRUCTION_LOG, parseRegistry(getCodeFromToken(par1))];
+  },
+  inc: (generatorState: GeneratorState) => {
+    const [, par1, par2] = generatorState.line;
+    return [Instruction.INSTRUCTION_INC, parseRegistry(getCodeFromToken(par1)), parseInt(getCodeFromToken(par2))];
+  },
+  jl: (generatorState: GeneratorState) => {
+    const [, par1] = generatorState.line;
+    return [Instruction.INSTRUCTION_INC];
+    // return [Instruction.INSTRUCTION_INC, getCodeFromToken(par1).slice(0, -1)];
+  },
+  halt: (generatorState: GeneratorState) => {
+    return [Instruction.INSTRUCTION_HALT];
   }
 };
 
@@ -41,61 +58,64 @@ function isInstruction(code: string | InstructionHandler): code is InstructionHa
   return code in InstructionHandlers;
 }
 
-const TokenHandlers = {
-  [Token.TOKEN_INSTRUCTION]: (tokenElement: TokenElement) => {
-    const instructionCode = getCodeFromToken(tokenElement);
+interface GeneratorState {
+  result: Array<number>,
+  line: TokenElement[],
+  labelMap: Map<string, number>
+}
+
+const generatorState: GeneratorState = {
+  result: [],
+  line: [],
+  labelMap: new Map<string, number>()
+};
+
+
+const programText = `
+push 100000
+load %rd
+log %rd
+halt
+`;
+
+
+const tokenStream = getTokenStream(programText);
+
+const ignorableTokens = [Token.TOKEN_WHITESPACE, Token.TOKEN_COMMENT, Token.TOKEN_COMMA];
+
+for (const tokenElement of tokenStream) {
+  if (ignorableTokens.includes(tokenElement.token)) {
+    continue;
+  }
+  if (tokenElement.token !== Token.TOKEN_NEWLINE) {
+    generatorState.line.push(tokenElement);
+    continue;
+  }
+  if (generatorState.line.length === 0) {
+    continue;
+  }
+
+  const [firstToken] = generatorState.line;
+  if (firstToken.token === Token.TOKEN_INSTRUCTION) {
+    const instructionCode = getCodeFromToken(firstToken);
     if (isInstruction(instructionCode)) {
-      return [InstructionHandlers[instructionCode](tokenElement)];
+      generatorState.result.push(...InstructionHandlers[instructionCode](generatorState));
     } else {
       panic(`Instruction not recognised: ${instructionCode}. location: ${tokenElement.startIndex}`);
     }
-  },
-  [Token.TOKEN_COMMENT]: (tokenElement: TokenElement) => {
-    return [];
-  },
-  [Token.TOKEN_WHITESPACE]: (tokenElement: TokenElement) => {
-    return [];
-  },
-  [Token.TOKEN_INVALID]: (tokenElement: TokenElement) => {
-    panic(`Invalid. location: ${tokenElement.startIndex}`);
-  },
-  [Token.TOKEN_LABEL]: (tokenElement: TokenElement) => {
-    return [];
-  },
-  [Token.TOKEN_COMMA]: (tokenElement: TokenElement) => {
-    return [];
-  },
-  [Token.TOKEN_REGISTER]: (tokenElement: TokenElement) => {
-    const code = getCodeFromToken(tokenElement);
-    switch (code) {
-    case '%rd':
-      return [Register.D];
-    default:
-      panic(`Register is not known: ${code}. location: ${tokenElement.startIndex}`);
-    }
-    return [];
-  },
-  [Token.TOKEN_CONST]: (tokenElement: TokenElement) => {
-    return [parseInt(getCodeFromToken(tokenElement))];
-  },
-  [Token.TOKEN_STACK]: (tokenElement: TokenElement) => {
-    return [];
-  },
-  [Token.TOKEN_NEWLINE]: (tokenElement: TokenElement) => {
-    return [];
+    generatorState.line = [];
+  } else if (firstToken.token === Token.TOKEN_LABEL) {
+    const code = getCodeFromToken(tokenElement).slice(0, -1);
+    generatorState.result.push(Instruction.INSTRUCTION_LABEL);
+    generatorState.labelMap.set(code, generatorState.result.length);
+  } else if (firstToken.token !== Token.TOKEN_COMMENT) {
+    panic(`Line needs to start with Instruction or Comment code: ${firstToken}. location: ${firstToken.startIndex}`);
   }
-};
-
-const parametres = [];
-
-const result = [];
-for (const tokenElement of tokenStream) {
-  result.push(...TokenHandlers[tokenElement.token](tokenElement));
 }
 
-console.log(tokenStream);
-console.log(result);
+console.log(generatorState);
 
-const instructions = Int32Array.from(result);
+const instructions = Int32Array.from(generatorState.result);
+
 const vm = new Vm(instructions);
 vm.run();
