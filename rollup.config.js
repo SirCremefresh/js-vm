7577f25c422925e472f34f2c30f1c170b2ddc45f
SirCremefresh <donato.wolfisberg@gmail.com>
2020-11-13 09:22:20 +0100

    moved stuff around

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
-import {terser} from 'rollup-plugin-terser';
+import { terser } from 'rollup-plugin-terser';
 import replace from '@rollup/plugin-replace';
 import copy from 'rollup-plugin-copy';
 
-const globalPlugins =  [
+const globalPlugins = [
   replace({
-    'process.env.NODE_ENV': JSON.stringify('production'),
+    'process.env.NODE_ENV': JSON.stringify('production')
   })
 ];
+
+const specificProject = process.env.SPECIFIC_PROJECT;
 
 const terserPlugin = terser({
   ecma: 2020,
@@ -16,35 +18,38 @@ const terserPlugin = terser({
 });
 
 const projects = [
-  {projectName: 'sample-lexer-program', copyPublic: true},
-  {projectName: 'sample-vm-program', copyPublic: false}
-].map(({projectName, copyPublic}) => ({
-  input: `dist/tsc/app/${projectName}/index.js`,
-  output: [
-    {
-      file: `dist/${projectName}/index.pretty.js`,
-      name: 'create_single_file',
-      format: 'es',
-    },
-    {
-      file: `dist/${projectName}/index.js`,
-      format: 'es',
-      name: 'optimize',
-      plugins: [
-        terserPlugin
-      ]
-    }
-  ],
-  plugins: [
-    ...globalPlugins,  
-    ...(copyPublic ? [
-      copy({
-        targets: [
-          { src: `src/app/${projectName}/public/*`, dest: `dist/${projectName}/` }
+  { projectName: 'web-ide', copyPublic: true },
+  { projectName: 'sample-vm-program', copyPublic: false },
+  { projectName: 'sample-code-generator', copyPublic: false }
+]
+  .filter(project => specificProject === undefined || project.projectName === specificProject)
+  .map(({ projectName, copyPublic }) => ({
+    input: `dist/tsc/app/${projectName}/index.js`,
+    output: [
+      {
+        file: `dist/${projectName}/index.pretty.js`,
+        name: 'create_single_file',
+        format: 'es'
+      },
+      {
+        file: `dist/${projectName}/index.js`,
+        format: 'es',
+        name: 'optimize',
+        plugins: [
+          terserPlugin
         ]
-      })
-    ]:[])
-  ]
-}));
+      }
+    ],
+    plugins: [
+      ...globalPlugins,
+      ...(copyPublic ? [
+        copy({
+          targets: [
+            { src: `src/app/${projectName}/public/*`, dest: `dist/${projectName}/` }
+          ]
+        })
+      ] : [])
+    ]
+  }));
 
 export default [...projects];
